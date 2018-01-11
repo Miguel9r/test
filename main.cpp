5ccbb2f308cea98c0ce9d55ccb80d98eccffd2d3
Miguel Rojas <mroja008@hammer.cs.ucr.edu>
2018-01-11 15:29:14 -0800

    added user input

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
+Switched to branch 'userinput'
+[mroja008@hammer firstrepo]$ git branch
+  master
+* userinput
+* [mroja008@hammer firstrepo]$ git status
+* # On branch userinput
+* nothing to commit, working directory clean
+* [mroja008@hammer firstrepo]$ vim main.cpp
+*
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "hello git" << std::endl;
-    return 1;
+	std::string name;
+	std::cout << "What is your name?" << std::endl;
+	std::cin >> name;
+	std::cout << "Hello " << name << "!" << std::endl;
+
+	return 1;
 }
 
